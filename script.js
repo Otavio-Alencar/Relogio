a176f8a924fa868e761edd0788e64f530c50744d
Otávio Alencar <oalencar32@gmail.com>
2024-05-02 21:41:37 -0300

    create analog clock

--- script.js
+++ script.js
@@ -8,9 +8,22 @@ function updateClock(){
     let hour = now.getHours()
     let minute = now.getMinutes()
     let seconds = now.getSeconds()
+
+
     digitalElement.innerHTML= `${fixZero(hour)}: ${fixZero(minute)}: ${fixZero(seconds)}`
+
+    let sDeg =  ((360/60) * seconds) - 90
+    sElement.style.transform = `rotate(${sDeg}deg)`
+
+    let mDeg = ((360/60) * minute) - 90
+    mElement.style.transform = `rotate(${mDeg}deg)`
+
+    let hDeg = ((360/12) * hour) - 90
+    hElement.style.transform = `rotate(${hDeg}deg)`
+
 }
 function fixZero(time){
     return time < 10 ? '0'+time : time
 }
 setInterval(updateClock,1000)
+updateClock()
